afd9cd44b0a040f884ba3613fd89d610948069f2
Marwan <emef4z@gmail.com>
2019-03-25 19:22:49 +0100

    Add Android Design Support Library

--- app/src/main/java/com/example/mf4z/fulfulde/MainActivity.java
+++ app/src/main/java/com/example/mf4z/fulfulde/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mf4z.fulfulde;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -19,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 
